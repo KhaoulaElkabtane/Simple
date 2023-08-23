ea06b3f4d958b1a55c9c372780b4003371ab8eb5
achraf-boug <achraf.bougadir@gmail.com>
2023-08-23 19:54:47 +0100

    memory_functions2.c

--- memory_functions2.c
+++ memory_functions2.c
@@ -2,17 +2,16 @@
 
 /**
  * _freemalloc - frees pointer and NULL adress
- * @frm : pointer to free
- *
- * Return : 1 if free or 0
+ * @frm: pointer to free
+ * Return: 1 if free or 0
  */
 int _freemalloc(void **frm)
 {
-   if (frm && *frm)
-   {
-      free(*frm);
-      *frm = NULL;
-      return (1);
-   }
-   return (0);
+	if (frm && *frm)
+	{
+		free(*frm);
+		*frm = NULL;
+		return (1);
+	}
+	return (0);
 }
