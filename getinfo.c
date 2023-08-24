5e7f7d96694eb8655bea57f04a3fcb5c39d9cef1
titamine <fatima.zohra.ennaji@gmail.com>
2023-08-24 19:43:14 +0100

    mod4

--- getinfo.c
+++ getinfo.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * clear_info - initializes info_t struct
+ * unsetInfo - initializes info_t struct
  * @info: struct address
  */
-void clear_info(info_t *info)
+void unsetInfo(infocmd *info)
 {
 	info->arg = NULL;
 	info->argv = NULL;
@@ -13,25 +13,25 @@ void clear_info(info_t *info)
 }
 
 /**
- * set_info - initializes info_t struct
- * @info: struct address
- * @av: argument vector
+ * setInfo - init struct infocmd
+ * @info: struct info cmd
+ * @a: init values
  */
-void set_info(info_t *info, char **av)
+void setInfo(infocmd *info, char **a)
 {
 	int i = 0;
 
-	info->fname = av[0];
+	info->fname = a[0];
 	if (info->arg)
 	{
-		info->argv = strtow(info->arg, " \t");
+		info->argv = strToWords(info->arg, " \t");
 		if (!info->argv)
 		{
 
 			info->argv = malloc(sizeof(char *) * 2);
 			if (info->argv)
 			{
-				info->argv[0] = _strdup(info->arg);
+				info->argv[0] = strduplication(info->arg);
 				info->argv[1] = NULL;
 			}
 		}
@@ -39,34 +39,34 @@ void set_info(info_t *info, char **av)
 			;
 		info->argc = i;
 
-		replace_alias(info);
-		replace_vars(info);
+		repAlias(info);
+		repVars(info);
 	}
 }
 
 /**
- * free_info - frees info_t struct fields
- * @info: struct address
- * @all: true if freeing all fields
+ * libInfo - free infocmd struct
+ * @info: pointer to struct
+ * @a: true if ok
  */
-void free_info(info_t *info, int all)
+void libInfo(infocmd *info, int a)
 {
-	ffree(info->argv);
+	strfree(info->argv);
 	info->argv = NULL;
 	info->path = NULL;
-	if (all)
+	if (a)
 	{
 		if (!info->cmd_buf)
 			free(info->arg);
 		if (info->env)
-			free_list(&(info->env));
+			freeList(&(info->env));
 		if (info->history)
-			free_list(&(info->history));
+			freeList(&(info->history));
 		if (info->alias)
-			free_list(&(info->alias));
-		ffree(info->environ);
+			freeList(&(info->alias));
+		strfree(info->environ);
 			info->environ = NULL;
-		bfree((void **)info->cmd_buf);
+		freemem((void **)info->cmd_buf);
 		if (info->readfd > 2)
 			close(info->readfd);
 		_putchar(BUF_FLUSH);
