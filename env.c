b8672ef7339d5e778f08b95095eacba3c8ff78b6
titamine <fatima.zohra.ennaji@gmail.com>
2023-08-24 18:42:35 +0100

    mod4

--- env.c
+++ env.c
@@ -1,32 +1,32 @@
 #include "shell.h"
 
 /**
- * _myenv - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * _myenv - prints the environment
+ * @info: Struct containing potential arguments
+ *
  * Return: Always 0
  */
-int _myenv(info_t *info)
+int curEnv(infocmd *info)
 {
-	print_list_str(info->env);
+	printList(info->env);
 	return (0);
 }
 
 /**
- * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
- * @name: env var name
+ * getVarEnv - gets the value of an environ variable
+ * @info: Structure containing potential arguments
+ * @name: environ name
  *
- * Return: the value
+ * Return: the environ value
  */
-char *_getenv(info_t *info, const char *name)
+char *getVarEnv(infocmd *info, const char *name)
 {
-	list_t *node = info->env;
+	liststr *node = info->env;
 	char *p;
 
 	while (node)
 	{
-		p = starts_with(node->str, name);
+		p = startsWith(node->s, name);
 		if (p && *p)
 			return (p);
 		node = node->next;
@@ -35,20 +35,19 @@ char *_getenv(info_t *info, const char *name)
 }
 
 /**
- * _mysetenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
+ * setEnvVal - set a environment variable
+ * @i: Structure containing potential arguments
+ *
+ *  Return: 0 if ok 1 if fails
  */
-int _mysetenv(info_t *info)
+int setEnvVal(infocmd *i)
 {
-	if (info->argc != 3)
+	if (i->argc != 3)
 	{
-		_eputs("Incorrect number of arguements\n");
+		erputs("Incorrect number of arguements\n");
 		return (1);
 	}
-	if (_setenv(info, info->argv[1], info->argv[2]))
+	if (setEnv(i, i->argv[1], i->argv[2]))
 		return (0);
 	return (1);
 }
@@ -59,34 +58,34 @@ int _mysetenv(info_t *info)
  *        constant function prototype.
  *  Return: Always 0
  */
-int _myunsetenv(info_t *info)
+int unsetEnvVal(infocmd *info)
 {
 	int i;
 
 	if (info->argc == 1)
 	{
-		_eputs("Too few arguements.\n");
+		erputs("Too few arguements.\n");
 		return (1);
 	}
 	for (i = 1; i <= info->argc; i++)
-		_unsetenv(info, info->argv[i]);
+		unsetEnv(info, info->argv[i]);
 
 	return (0);
 }
 
 /**
- * populate_env_list - populates env linked list
+ * initEnvList - init list
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  * Return: Always 0
  */
-int populate_env_list(info_t *info)
+int initEnvList(infocmd *info)
 {
-	list_t *node = NULL;
+	liststr *node = NULL;
 	size_t i;
 
 	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
+		addEndNode(&node, environ[i], 0);
 	info->env = node;
 	return (0);
 }
--- getenv.c
+++ getenv.c
@@ -4,13 +4,13 @@
  * get_environ - returns the string array copy of our environ
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
- * Return: Always 0
+ * Return: environ value
  */
-char **get_environ(info_t *info)
+char **getEnv(infocmd *info)
 {
 	if (!info->environ || info->env_changed)
 	{
-		info->environ = list_to_strings(info->env);
+		info->environ = listStrings(info->env);
 		info->env_changed = 0;
 	}
 
@@ -18,27 +18,27 @@ char **get_environ(info_t *info)
 }
 
 /**
- * _unsetenv - Remove an environment variable
+ * unsetEnv - Remove an environment variable
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  *  Return: 1 on delete, 0 otherwise
- * @var: the string env var property
+ * @v: the string env var property
  */
-int _unsetenv(info_t *info, char *var)
+int unsetEnv(infocmd *info, char *v)
 {
-	list_t *node = info->env;
+	liststr *node = info->env;
 	size_t i = 0;
 	char *p;
 
-	if (!node || !var)
+	if (!node || !v)
 		return (0);
 
 	while (node)
 	{
-		p = starts_with(node->str, var);
+		p = startsWith(node->s, v);
 		if (p && *p == '=')
 		{
-			info->env_changed = delete_node_at_index(&(info->env), i);
+			info->env_changed = delNodeIndex(&(info->env), i);
 			i = 0;
 			node = info->env;
 			continue;
@@ -58,36 +58,36 @@ int _unsetenv(info_t *info, char *var)
  * @value: the string env var value
  *  Return: Always 0
  */
-int _setenv(info_t *info, char *var, char *value)
+int setEnv(infocmd *info, char *v, char *val)
 {
-	char *buf = NULL;
-	list_t *node;
+	char *buffer = NULL;
+	liststr *node;
 	char *p;
 
-	if (!var || !value)
+	if (!v || !val)
 		return (0);
 
-	buf = malloc(_strlen(var) + _strlen(value) + 2);
-	if (!buf)
+	buffer = malloc(strlength(v) + strlength(val) + 2);
+	if (!buffer)
 		return (1);
-	_strcpy(buf, var);
-	_strcat(buf, "=");
-	_strcat(buf, value);
+	strcopy(buffer, v);
+	strCat(buffer, "=");
+	strCat(buffer, val);
 	node = info->env;
 	while (node)
 	{
-		p = starts_with(node->str, var);
+		p = startsWith(node->s, v);
 		if (p && *p == '=')
 		{
-			free(node->str);
-			node->str = buf;
+			free(node->s);
+			node->s = buffer;
 			info->env_changed = 1;
 			return (0);
 		}
 		node = node->next;
 	}
-	add_node_end(&(info->env), buf, 0);
-	free(buf);
+	addEndNode(&(info->env), buffer, 0);
+	free(buffer);
 	info->env_changed = 1;
 	return (0);
 }
